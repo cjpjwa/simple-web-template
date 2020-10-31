fb13390b9341270654ccbdcf7e937678460a3b4e
Peter Janko <contact@cjpj.de>
2020-10-31 15:56:51 +0100

    Inititial version
    Upload of initlal version

---
+++ js/simpleweb.js
@@ -0,0 +1,37 @@
+//########################################################################################
+//simple web shell template, javascript, 31.10.2020
+//########################################################################################
+//strict mode
+"use strict";
+//########################################################################################
+var mobileWidth   = 800;		//integer, css value in px: below switch to dropdown menu
+var mobileNavi    = false;		//boolean, dropdown status: false = hidden, true = shown)
+//########################################################################################
+//show/hide mobile navigation dropdown menue, rotate hamburger icon
+function showHideNav() {
+	if (mobileNavi === false) {
+		mobileNavi = true;
+		document.getElementById("navi").style.marginTop = "12px";
+		document.getElementById("mobile").style.transform = "rotate(90deg)";
+	} else {
+		mobileNavi = false;
+		document.getElementById("navi").style.marginTop = "-500px";
+		document.getElementById("mobile").style.transform = "rotate(0deg)";
+	}
+}
+
+//########################################################################################
+//reset navigation menue when resizing screen (or switch orientation)
+function resetNav() {
+	if (window.innerWidth > mobileWidth) {
+		document.getElementById("navi").style.marginTop = "0px";
+		mobileNavi = false;
+	} else {
+		document.getElementById("navi").style.marginTop = "-500px";
+		document.getElementById("mobile").style.transform = "rotate(180deg)";
+	}
+}
+
+//########################################################################################
+//trigger reset navigation
+window.addEventListener("resize", resetNav);
